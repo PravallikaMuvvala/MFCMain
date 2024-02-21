75d1a89584959972881b660456ddc6817904ecac
PravallikaMuvvala <pravallikamuvvala577@gmail.com>
2024-02-21 08:52:29 +0530

    Add project files.

---
+++ MFCMain/Source.cpp
@@ -0,0 +1,20 @@
+#include<afxwin.h>
+
+class myframe :public CFrameWnd {
+public:
+	myframe() {
+		Create(0, "title");
+	}
+};
+class myapp :public CWinApp {
+public:
+	BOOL InitInstance() {
+		myframe* pframe = new myframe;
+		m_pMainWnd = pframe;
+		pframe->ShowWindow(1);
+
+		return TRUE;
+	}
+};
+
+myapp obj;
